a9852e7a6164bb5b72ac80cce8c3a5f461a1b7dc
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-03 18:14:55 +0300

    Refactor code with step 4 requirements

--- main.py
+++ main.py
@@ -12,38 +12,37 @@ BASE_URL = "https://api-ssl.bitly.com/v4"
 def get_user_info() -> dict:
     """Returns information for the current authenticated user"""
 
-    url = f"{BASE_URL}/user"
+    endpoint = f"{BASE_URL}/user"
     headers = {
         "Authorization": f"Bearer {TOKEN}",
     }
 
-    response = requests.get(url=url, headers=headers)
+    response = requests.get(url=endpoint, headers=headers)
     response.raise_for_status()
 
     return response.json()
 
 
-def shorten_url(long_url: str) -> str:
+def shorten_link(token: str, url: str) -> str:
     """Converts a long url to a Bitlink"""
 
-    url = f"{BASE_URL}/shorten"
+    endpoint = f"{BASE_URL}/shorten"
     headers = {
-        "Authorization": f"Bearer {TOKEN}",
+        "Authorization": f"Bearer {token}",
         "Content-Type": "application/json",
     }
 
     data = {
-        "long_url": long_url,
+        "long_url": url,
         "domain": "bit.ly",
     }
 
-    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response = requests.post(url=endpoint, headers=headers, data=json.dumps(data))
     response.raise_for_status()
 
     return response.json().get("id")
 
 
 if __name__ == "__main__":
-    # print(get_user_info())
     user_url = input("Enter full url to be shorten... ")
-    print(shorten_url(user_url))
+    print("Битлинк", shorten_link(TOKEN, user_url))
