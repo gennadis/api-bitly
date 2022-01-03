871c7336f9075b360a441c09e04561bd4161557c
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-03 18:05:36 +0300

    Add a function for shorting url via Bitly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from dotenv import load_dotenv
 
@@ -22,5 +23,27 @@ def get_user_info() -> dict:
     return response.json()
 
 
+def shorten_url(long_url: str) -> str:
+    """Converts a long url to a Bitlink"""
+
+    url = f"{BASE_URL}/shorten"
+    headers = {
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json",
+    }
+
+    data = {
+        "long_url": long_url,
+        "domain": "bit.ly",
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response.raise_for_status()
+
+    return response.json().get("id")
+
+
 if __name__ == "__main__":
-    print(get_user_info())
+    # print(get_user_info())
+    user_url = input("Enter full url to be shorten... ")
+    print(shorten_url(user_url))
