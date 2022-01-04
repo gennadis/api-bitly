e8a7c13cc2a390860ff479a3b25d5036bb04d83f
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-04 20:57:13 +0300

    Remove deprecated functionality

--- main.py
+++ main.py
@@ -9,20 +9,6 @@ TOKEN = os.environ.get("BITLY_TOKEN")
 BASE_URL = "https://api-ssl.bitly.com/v4"
 
 
-def get_user_info() -> dict:
-    """Returns information for the current authenticated user"""
-
-    endpoint = f"{BASE_URL}/user"
-    headers = {
-        "Authorization": f"Bearer {TOKEN}",
-    }
-
-    response = requests.get(url=endpoint, headers=headers)
-    response.raise_for_status()
-
-    return response.json()
-
-
 def shorten_link(token: str, url: str) -> str:
     """Converts a long url to a Bitlink"""
 
