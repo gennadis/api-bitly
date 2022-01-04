ae190284fc3367a9be2f7834814115903a62d74d
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-04 21:33:33 +0300

    Remove global vars from functions for maintainability

--- main.py
+++ main.py
@@ -14,69 +14,70 @@ ENDPOINTS = {
 }
 
 
-def shorten_link(token: str, url: str) -> str:
+def shorten_link(token: str, base_url: str, endpoint: str, link: str) -> str:
     """Converts a long url to a Bitlink"""
 
-    endpoint = f"{BASE_URL}/shorten"
+    url = base_url + endpoint
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json",
     }
 
     data = {
-        "long_url": url,
+        "long_url": link,
         "domain": "bit.ly",
     }
 
-    response = requests.post(url=endpoint, headers=headers, data=json.dumps(data))
+    response = requests.post(url=url, headers=headers, data=json.dumps(data))
     response.raise_for_status()
     bitlink = response.json().get("id")
 
     return bitlink
 
 
-def count_clicks(token: str, bitlink: str) -> int:
+def count_clicks(token: str, base_url: str, endpoint: str, bitlink: str) -> int:
     """Returns the click counts for a Bitlink"""
 
-    endpoint = f"{BASE_URL}/bitlinks/{bitlink}/clicks/summary"
+    url = base_url + endpoint.format(bitlink)
     headers = {
         "Authorization": f"Bearer {token}",
     }
 
-    response = requests.get(url=endpoint, headers=headers)
+    response = requests.get(url=url, headers=headers)
     response.raise_for_status()
     clicks: int = response.json().get("total_clicks")
 
     return clicks
 
 
-def is_bitlink(url: str) -> bool:
+def is_bitlink(base_url: str, endpoint: str, link: str) -> bool:
     """Check if URL is a Bitlink"""
 
-    parsed_url = urllib.parse.urlparse(url)
-    bitlink = f"{parsed_url.netloc}{parsed_url.path}"
-    endpoint = f"{BASE_URL}/bitlinks/{bitlink}"
+    parsed_link = urllib.parse.urlparse(link)
+    bitlink = parsed_link.netloc + parsed_link.path
+
+    url = base_url + endpoint.format(bitlink)
     headers = {
         "Authorization": f"Bearer {TOKEN}",
     }
 
-    response = requests.get(url=endpoint, headers=headers)
+    response = requests.get(url=url, headers=headers)
 
     return response.ok
 
 
 def main():
-    user_url = input("Please enter your URL: ")
-    if is_bitlink(user_url):
+    user_link = input("Please enter your URL: ")
+    if is_bitlink(BASE_URL, ENDPOINTS["is_bitlink"], user_link):
         try:
-            count = count_clicks(TOKEN, user_url)
+            count = count_clicks(TOKEN, BASE_URL, ENDPOINTS["clicks"], user_link)
         except requests.exceptions.HTTPError as e:
             print(f"Bitlink validation error - {e}")
         else:
             print("Click counts:", count)
     else:
         try:
-            bitlink = shorten_link(TOKEN, user_url)
+            bitlink = shorten_link(TOKEN, BASE_URL, ENDPOINTS["shorten"], user_link)
         except requests.exceptions.HTTPError as e:
             print(f"URL validation error - {e}")
         else:
